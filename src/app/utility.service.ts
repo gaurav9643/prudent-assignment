205603db1b0be4238b0637a61a2f757a9d8d824f
gauravrathore9643 <gaurav@encriss.com>
2022-08-21 13:58:45 +0530

    [Exercise 3] : Correct punctuation.

--- src/app/utility.service.ts
+++ src/app/utility.service.ts
@@ -7,8 +7,17 @@ export class UtilsService {
   constructor() {}
 
   wordAutoCorrect(term: string, dictionary: string[]): string[] {
-    return dictionary.filter((word: string) =>
-      word.toLowerCase().startsWith(term.toLowerCase())
-    );
+    return dictionary.filter((word: string) => {
+      let isMatch = this.matchWord(word, term);
+      if (word.match(/['-/]/)) {
+        const _word = word.replace(/['-/]/, '');
+        isMatch = this.matchWord(_word, term);
+      }
+      return isMatch;
+    });
+  }
+
+  matchWord(str1: string, str2: string): boolean {
+    return str1.toLowerCase().startsWith(str2.toLowerCase());
   }
 }
